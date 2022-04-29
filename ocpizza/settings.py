7c4224e41717ae0316d38bc68f7f8cf52ca6fa86
dylamn <dylan.vallee@hotmail.com>
2022-04-29 14:46:28 +0200

    Added docs

--- ocpizza/settings.py
+++ ocpizza/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e5_49w1_xbh&ow6(3(iedsqos7zw*xx55my3@-+0-4)-fn5yv6'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
